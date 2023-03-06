6fa565e4750d9d8881eb1bdb5d71bc0dfd73cdb3
GoldenJaden <yaroslav-gusev-00@mail.ru>
2023-03-06 20:51:06 +0300

    megalast

--- Algorythms/2 Семестр/Lab 2 Hash/crc32.py
+++ Algorythms/2 Семестр/Lab 2 Hash/crc32.py
@@ -3,14 +3,13 @@ text = 'Hello world'
 
 def crc32(str_input: str) -> str:
     msg = ''
-    for s in str_input:  # Проходимся по каждому символу в строке
+    for s in str_input:
         msg += bin(ord(s))[2:]  # Преобразуем символ в его ASCII-код, затем в двоичный вид и добавляем к msg
 
     g = '100110000010001110110110111'  # Задаем полином-генератор CRC32 в двоичном виде
-    msg = msg + '0' * (len(g) - 1)  # Добавляем (len(g) - 1) нулей к msg
+    msg = msg + '0' * (len(g) - 1)
 
-    block = msg[0:len(g)]  # Инициализируем первый блок первыми (len(g)) битами сообщения
-    print(msg, block)
+    block = msg[0:len(g)]
     i = len(g) - 1
 
     # Пока i меньше длины сообщения, выполняем XOR блока с полиномом-генератором
@@ -23,8 +22,6 @@ def crc32(str_input: str) -> str:
             if i < len(msg):
                 block += msg[i]
             else:
-                # Если мы достигли конца сообщения, возвращаем хеш CRC32 в шестнадцатеричном виде
                 return hex(int(block, 2))
 
-# Вызываем функцию crc32 для переменной text и печатаем результат
 print(crc32(text))
