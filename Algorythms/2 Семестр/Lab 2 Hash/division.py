9b72d86e062a6940f30894c6081aa1cc0b6ee359
GoldenJaden <yaroslav-gusev-00@mail.ru>
2023-03-06 20:08:29 +0300

    final

--- Algorythms/2 Семестр/Lab 2 Hash/division.py
+++ Algorythms/2 Семестр/Lab 2 Hash/division.py
@@ -1,23 +1,20 @@
-def build_hash_table_div(inpt_str: str) -> list:
-    keys = list(set(inpt_str)) # Все символы, встречающиеся в нашей строке
+def hash_division(keys):
     n = len(keys)
-    while True: # Ищем первое простое число, больше длины входных данных, чтобы уменьшить вероятность коллизии
-        found = True
-        for i in range(2, int(n ** 0.5 + 1)):
-            if n % i == 0:
-                found = False
-                break
-        if found == True:
-            break
-        n += 1
     hash_table = [None for i in range(n)]
-    for s in keys: # Заполняем таблицу
-        if hash_table[ord(s) % n] is None:
-            hash_table[ord(s) % n] = [s]
+    for i in range(n):
+        if hash_table[keys[i] % n] is None:
+            hash_table[keys[i] % n] = [keys[i]]
         else:
-            hash_table[ord(s) % n].append(s)
+            hash_table[keys[i] % n].append(keys[i])
     return hash_table
 
-in_str = "Hello world"
-hashed_table = build_hash_table_div(in_str)
-print(hashed_table)
+def display_hashtable(table):
+    for i in range(len(table)):
+        if table[i] is not None:
+            print(i, '-->', *table[i])
+
+
+keys = [11, 122, 123, 154, 15, 217, 108, 195, 124, 4, 1]
+
+hash_table = hash_division(keys)
+display_hashtable(hash_table)
